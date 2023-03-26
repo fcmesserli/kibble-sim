266046c81fa2f6beb1f929fdca2edba50f8fc63f
fcmesserli <129009442+fcmesserli@users.noreply.github.com>
2023-03-27 11:26:02 +1300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="kibble-sim",
+    url="https://github.com/fcmesserli/kibble-sim",
+    author="John Ladan",
+    author_email="jladan@uwaterloo.ca",
+    # Needed to actually package something
+    packages=["moving_mode"],
+    # Needed for dependencies
+    install_requires=["numpy"],
+    version="0.1",
+    license="MIT",
+    description="An example of a python package from pre-existing code",
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
