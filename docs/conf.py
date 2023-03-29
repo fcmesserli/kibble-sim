01cad0b873bab35c42c00c5585a3345e96ec8876
fcmesserli <129009442+fcmesserli@users.noreply.github.com>
2023-03-29 13:05:49 +1300

    Use google style docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -51,8 +51,8 @@ autosummary_generate = True
 autoclass_content = "both"
 
 # Napoleon settings
-napoleon_google_docstring = False
-napoleon_numpy_docstring = True
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = False
